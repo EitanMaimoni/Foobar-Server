b8306cea712ad5e422631abd05a373386b44be52
Eitan <eitanmaimoni1@gmail.com>
2024-03-05 20:08:53 +0200

    recover deleted code

--- routes/post.js
+++ routes/post.js
@@ -5,6 +5,22 @@ const userController = require('../controllers/user');
 const express = require('express');
 var router = express.Router();
 
-router.route('/:id/posts/:pid').delete(authenticateToken, postController.deletePost)
+router.route('/').get(authenticateToken,postController.getFriendsPosts)
+router.route('/nickname/:id').get(userController.getUsernickname)
+router.route('/profile/:id').get(userController.getUserImage)
+router.route('/get').get(authenticateToken, postController.getPosts);
+router.route('/like/:id').put(authenticateToken, postController.likePost)
+router.route('/:id').get(postController.getPost)
+        .patch(postController.updatePost)
+router.route('/uploadImage/:id').patch(authenticateToken, postController.updateImage)
+router.route('/edit/:id').get(authenticateToken, postController.checkIfAuth)
+
+router.route('/comment/like/:postid/:commentid').put(authenticateToken, postController.likeComment)
+router.route('/comment/edit/:postid/:commentname').get(authenticateToken, postController.checkIfAuthComment)
+router.route('/comment/edit/:postid/:commentid').patch(authenticateToken, postController.updateComment)
+router.route('/comment/:id').post(authenticateToken, postController.addComment)
+router.route('/comment/:postid/:commentid').delete(authenticateToken, postController.deleteComment)
+
+router.route('/delete/:id').delete(authenticateToken, postController.deletePost)
 
 module.exports = router;
